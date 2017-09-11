8bd801a537049d767ebccf83b213d80fc4ecec60
qtpsudhakar <sudhakar@qtpsudhakar>
2017-09-11 16:21:39 +0530

    third change

--- ibmtraining/src/test/java/com/tng/ApsrtcSearch_DDT.java
+++ ibmtraining/src/test/java/com/tng/ApsrtcSearch_DDT.java
@@ -23,6 +23,7 @@ public class ApsrtcSearch_DDT {
 	@BeforeClass
 	public void openApplication() {
 		System.out.println("First change");
+		System.out.println("second change");
 		driver = DriverFactory.getDriver("chrome");
 
 		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
